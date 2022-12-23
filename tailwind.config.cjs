0f7afb3619df0c51d3b383f6043efb8b9f1ff904
caduxl007 <eduardo2020dev@gmail.com>
2022-12-23 16:27:09 -0300

    chore: add config colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,28 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: 'Saira, sans-serif'
+      },
+      colors: {
+        white: '#fffff',
+
+        blue: {
+          700: '#115D8C',
+          800: '#123952',
+        },
+        green: {
+          100: '#DCF5DD',
+          400: '#51B853'
+        },
+        red: {
+          500: '#DE3838',
+        },
+        gray: {
+          200: '#F5F8FA',
+          600: '#A0ACB3',
+          500: '#DEDEDE',
+          700: '#737380',
+          800: '#617480',
+        }
       }
     },
   },
