00d4391c1c59139c1e2e6571539113c5232b79a3
caduxl007 <eduardo2020dev@gmail.com>
2022-12-29 00:21:36 -0300

    feat: create hook useProducts

---
+++ src/services/hooks/useProducts.ts
@@ -0,0 +1,46 @@
+import { useQuery } from 'react-query';
+import { api } from '../api';
+
+export type Product = {
+  id: number;
+  name: string;
+  imageUrl: string;
+  category: string;
+  description: string;
+  price: number;
+  createdAt: string;
+};
+
+type GetUsersResponse = {
+  products: Product[];
+  totalCount: number;
+};
+
+type GetUsersRequest = {
+  page: number;
+};
+
+export async function getProducts({
+  page,
+}: GetUsersRequest): Promise<GetUsersResponse> {
+  const { data, headers } = await api.get('products', {
+    params: {
+      _page: page,
+      _limit: 10,
+    },
+  });
+
+  const products = data;
+  const totalCount = Number(headers['x-total-count']);
+
+  return {
+    products,
+    totalCount,
+  };
+}
+
+export function useProducts(request: GetUsersRequest) {
+  return useQuery(['products', request], () => getProducts(request), {
+    staleTime: 1000 * 60, // 1 minuto
+  });
+}
