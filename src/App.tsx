20d7c1b9e36981af8d2c5fe7914ef911184f29d0
caduxl007 <eduardo2020dev@gmail.com>
2023-01-09 22:59:00 -0300

    feat: persist cart in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './Router';
+import { persistStore } from 'redux-persist';
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { queryClient } from './lib/queryClient';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistor = persistStore(store)
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </PersistGate>
 
         <ReactQueryDevtools />
       </Provider>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PURGE } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import cart, { CartState } from './modules/cart';
 
 export interface State {
   cart: CartState;
 }
 
-export const store = configureStore({
-  reducer: {
-    cart,
-  },
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+};
+
+export const rootReducers = combineReducers({
+  cart,
 });
 
+const persistReducers = persistReducer(persistConfig, rootReducers);
+
+export const store = configureStore({
+  reducer: persistReducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PURGE],
+      },
+    }),
+});
+setupListeners(store.dispatch);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
